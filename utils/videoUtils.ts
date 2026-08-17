c1ba2e9d8c2a5758557c99e284ffba61fb593bf5
Rayner Seah <raynerseah@google.com>
2026-08-17 12:01:25 +0000

    fix(audio): set default gameplay volume to 50%

--- utils/videoUtils.ts
+++ utils/videoUtils.ts
@@ -527,7 +527,7 @@ export const compositePipVideo = async (
   const bgGain = audioCtx.createGain();
   const overlayGain = audioCtx.createGain();
 
-  bgGain.gain.value = typeof volumes?.gameplay === 'number' ? volumes.gameplay : 0.45; 
+  bgGain.gain.value = typeof volumes?.gameplay === 'number' ? volumes.gameplay : 0.5; 
   overlayGain.gain.value = typeof volumes?.streamer === 'number' ? volumes.streamer : 1.0;
 
   bgSource.connect(bgGain);
